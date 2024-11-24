b7bcdee33c1e425a3fee69fa926b8c3a1f22d617
yxqxq <hciu@ust.hk>
2024-11-25 03:54:12 +0800

    Update Login and Main for Student

--- src/main/java/comp3111/examsystem/controller/StudentLoginController.java
+++ src/main/java/comp3111/examsystem/controller/StudentLoginController.java
@@ -1,11 +1,15 @@
 package comp3111.examsystem.controller;
 
 import comp3111.examsystem.Main;
+import comp3111.examsystem.entity.Student;
+import comp3111.examsystem.service.StudentService;
+import comp3111.examsystem.service.internal.StudentServiceImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -22,25 +26,96 @@ public class StudentLoginController implements Initializable {
     @FXML
     private PasswordField passwordTxt;
 
-    public void initialize(URL location, ResourceBundle resources) {
+    private int studentid;
 
+    private final StudentService studentService = new StudentServiceImpl();
+
+    public void initialize(URL location, ResourceBundle resources) {
     }
+
+    //
+    public boolean accountExists(String username) {
+        for (Student student : studentService.getAllStudents()) {
+            if (student.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Student getStudentByUsername(String username) {
+        for (Student student : studentService.getAllStudents()) {
+            if (student.getUsername().equals(username)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public boolean checkLogin() {
+        if (accountExists(usernameTxt.getText())) {
+            Student student = getStudentByUsername(usernameTxt.getText());
+            if (student != null && student.Check_password(passwordTxt.getText())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @FXML
     public void login(ActionEvent e) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentMainUI.fxml"));
-        Stage stage = new Stage();
-        stage.setTitle("Hi " + usernameTxt.getText() +", Welcome to HKUST Examination System");
-        try {
-            stage.setScene(new Scene(fxmlLoader.load()));
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        if (checkLogin()) {
+            studentid = studentService.login(usernameTxt.getText(), passwordTxt.getText());
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentMainUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Hi " + usernameTxt.getText() + ", Welcome to HKUST Examination System");
+
+
+            try {
+                stage.setScene(new Scene(fxmlLoader.load()));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            StudentMainController controller = fxmlLoader.getController();
+            controller.setUsername(usernameTxt.getText());
+            controller.setID(studentid);
+
+            stage.show();
+            ((Stage) ((Button) e.getSource()).getScene().getWindow()).close();
         }
-        stage.show();
-        ((Stage) ((Button) e.getSource()).getScene().getWindow()).close();
+        else {
+            ErrorPopupController.Error_Popup("Username/Password incorrect.");
+        }
     }
 
     @FXML
     public void register() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentRegisterUI.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Student Register");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void backToMain() {
+        try {
+            Stage currentStage = (Stage) usernameTxt.getScene().getWindow();
+            currentStage.close();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Select Login");
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/comp3111/examsystem/controller/StudentMainController.java
+++ src/main/java/comp3111/examsystem/controller/StudentMainController.java
@@ -1,31 +1,139 @@
 package comp3111.examsystem.controller;
 
 import comp3111.examsystem.Main;
-import javafx.event.Event;
+import comp3111.examsystem.entity.Examination;
+import comp3111.examsystem.service.ExaminationService;
+import comp3111.examsystem.service.internal.ExaminationServiceImpl;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class StudentMainController implements Initializable {
     @FXML
-    ComboBox<String> examCombox;
+    private ComboBox<Examination> examCombox;
 
+    private final ExaminationService examinationService = new ExaminationServiceImpl();
+
+    @FXML
+    private VBox mainbox;
+
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private int studentid;
+
+    public void setID(int studentid) {
+        this.studentid = studentid;
+    }
+
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadPublishedExams();
+
+        examCombox.setCellFactory(lv -> new ListCell<Examination>() {
+            @Override
+            protected void updateItem(Examination exam, boolean empty) {
+                super.updateItem(exam, empty);
+                if (empty || exam == null) {
+                    setText(null);
+                } else {
+                    setText(exam.getCourseID() + " - " + exam.getExamName());
+                }
+            }
+        });
+
+        examCombox.setButtonCell(new ListCell<Examination>() {
+            @Override
+            protected void updateItem(Examination exam, boolean empty) {
+                super.updateItem(exam, empty);
+                if (empty || exam == null) {
+                    setText(null);
+                } else {
+                    setText(exam.getCourseID() + " - " + exam.getExamName());
+                }
+            }
+        });
     }
 
     @FXML
-    public void openExamUI() {
+    private void loadPublishedExams() {
+        List<Examination> exams = examinationService.getAllExaminations();
+        List<Examination> publishedExams = exams.stream()
+                .filter(Examination::getPublish)
+                .collect(Collectors.toList());
+        examCombox.getItems().addAll(publishedExams);
+    }
+
+
+
+    @FXML
+    private void openExamUI() {
+        Examination selectedExam = examCombox.getSelectionModel().getSelectedItem();
+        if (selectedExam != null) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentExamStartUI.fxml"));
+                Stage stage = new Stage();
+                stage.setTitle("Exam: " + selectedExam.getExamName());
+                stage.setScene(new Scene(fxmlLoader.load()));
+
+                // Get the controller and set the current exam
+                StudentExamStartController controller = fxmlLoader.getController();
+                controller.setExamDetails(selectedExam);
+                controller.setUsername(username);
+                controller.setID(studentid);
+
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
     public void openGradeStatistic() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentGradeStatistics.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Grade Statistics");
+            stage.setScene(new Scene(fxmlLoader.load()));
+
+            System.out.println("SetStats Username: " + username);
+            StudentGradeStatisticsController controller = fxmlLoader.getController();
+            controller.setUsername(username);
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void logout() {
+        try {
+            Stage currentStage = (Stage) mainbox.getScene().getWindow();
+            currentStage.close();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginUI.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
